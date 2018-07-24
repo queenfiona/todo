9f1c7b977ae6a5130b4db4fdec9cce63b5a9e772
queenfiona <fionaitya@gmail.com>
2018-07-24 14:02:27 +0300

    changes in .sh 2

--- pfinal.sh
+++ pfinal.sh
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
 export DATABASE_URL=postgres://app:app@localhost:5432/pfinal
-=======
-<<<<<<< HEAD
-export DATABASE_URL=postgres://app:app@localhost:5432/pfinal
-=======
-export DATABASE_URL = postgres://app:app@localhost:5432/pfinal
->>>>>>> f51b25bc7b94c289bd67116a42a5cda7deb5ace1
->>>>>>> 1f046ba34be5470012ae034ff2ae71e28a7aad3e
+
